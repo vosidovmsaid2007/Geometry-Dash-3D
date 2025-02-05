22740ebcc5a47f9cab5bb8bd994e0af026e25073
vosidovmsaid2007 <msaidvosidov@gmail.com>
2025-02-05 13:40:07 +0500

    Added PostProcessing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,130 +3,51 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Enum")] [SerializeField] private float currentMoveSpeed = 1f;
-
-    [Header("References")] 
-    [SerializeField] private LayerMask groundMask;
-    [SerializeField] private Transform playerSprite;
-    [SerializeField] private SpriteRenderer sprite;
-
-    [Header("Settings")] 
-    [SerializeField] private float[] speedValues = { };
-    [SerializeField] private float jumpForce = 10f;
-    [SerializeField] private float rotationSpeed = 5f;
-    [SerializeField] private float groundCheckRadius;
-    [SerializeField] private float shipGravityScale;
-    [SerializeField] private float cubeGravityScale;
+    [SerializeField] private float moveSpeed = 1f, jumpForce = 10f;
+    [SerializeField] private float fallMultuplier = 2.5f, lowJumpMultiplier = 2f;
 
     private Rigidbody rb;
-    private int _gravity = 1;
+    private bool doJump = false;
+    private bool isJumping = false;
 
-    private void Start()
+    void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = transform.GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    void Update()
     {
-        HandleMovement();
-        LimitFallSpeed();
-        // HandleOnWallHit();
-        // if (currentGameMode == GameModes.Cube)
-        // {
-        //     Cube();
-        //     sprite.color = Color.white;
-        // }
-        // else if (currentGameMode == GameModes.Ship)
-        // {
-        //     Ship();
-        //     sprite.color = Color.black;
-        // }
-        //HandleGameModeBehaviour();
-    }
-
-    private void HandleMovement()
-    {
-        var moveSpeedIndex = (int)currentMoveSpeed; 
-        transform.position += Vector3.right * (speedValues[0] * Time.deltaTime);
-    }
-
-    /**
-    private void HandleGameModeBehaviour()
-    {
-         Invoke(currentGameMode.ToString(), 0); 
-    }
-    */
-    // private void HandleOnWallHit()
-    // {
-    //     if (TouchWall())
-    //     {
-    //         SceneLoader.ReloadScene();
-    //     }
-    // }
-    
-    private void Cube()
-    {
-        if (OnGrounded())
-        {
-        
-            var rotation = playerSprite.rotation.eulerAngles;
-            rotation.z = Mathf.Round(rotation.z / 90) * 90;
-            playerSprite.rotation = Quaternion.Euler(rotation); // Complete the rotation
-            if (Input.GetMouseButton(0))
-            {
-                Jump();
-            }
-        }
-        else
-        {
-            playerSprite.Rotate(Vector3.back, _gravity * rotationSpeed * Time.deltaTime);
-        }
-
-        rb.gravityScale = cubeGravityScale * _gravity;
-    }
-    
-    private void Ship()
-    { 
-        playerSprite.rotation = Quaternion.Euler(0, 0, rb.velocity.y * 2);
-
-        if (Input.GetMouseButton(0))
-            rb.gravityScale = -shipGravityScale;
-        else
-            rb.gravityScale = shipGravityScale;
-        rb.gravityScale *= _gravity;
-    }
-    
-    private void Jump()
-    {
-        rb.velocity = Vector2.zero;
-        rb.AddForce(Vector2.up * jumpForce * _gravity, ForceMode2D.Impulse);
-    }
-    private bool OnGrounded()
-    {
-        Vector3 downOffset = Vector3.down * _gravity * 0.5f;
-        Vector2 rightSize = Vector2.right * 1.1f;
-        Vector2 upSize = Vector2.up * groundCheckRadius;
-
-        Vector3 position = transform.position + downOffset;
-        Vector2 size = rightSize + upSize;
-
-        return Physics2D.OverlapBox(position, size, 0, groundMask);
-    }
-    private bool TouchWall()
-    {
-        Vector2 offset = Vector2.right * 0.55f;
-        Vector2 size = new Vector2(groundCheckRadius * 2, 0.8f);
-        Vector2 position = (Vector2)transform.position + offset;
-
-        return Physics2D.OverlapBox(position, size, 0, groundMask);
-    }
-
-    private void LimitFallSpeed()
-    {
-        if ((rb.velocity.y  * _gravity) < -24.2f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, -24.2f * _gravity);
+        if(Input.GetButtonDown("Jump") && !isJumping){
+            doJump = true;
+            isJumping = false;
         }
     }
+
+    void FixedUpdate()
+    {
+        Vector3 moveVect = Vector3.forward;
+        moveVect = moveVect.normalized * moveSpeed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + moveVect);
+
+        if(doJump){
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            doJump = false;
+        }
+
+        if(rb.velocity.y < 0){
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultuplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")){
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter(Collision other){
+        if(other.gameObject.CompareTag("Ground")){
+            isJumping = false;
+        }
+    }
+
     
 }
